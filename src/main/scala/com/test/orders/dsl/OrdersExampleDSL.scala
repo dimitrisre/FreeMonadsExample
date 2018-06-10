7d2efd36b32c38d2f5b5e1b441def49148b29a6b
Dimitris Tsolis <dimitris.re@gmail.com>
2018-06-10 20:18:28 +0300

    Adding error support

--- src/main/scala/com/test/orders/dsl/OrdersExampleDSL.scala
+++ src/main/scala/com/test/orders/dsl/OrdersExampleDSL.scala
@@ -33,8 +33,8 @@ object OrdersExampleDSL {
   def smartTraide: OrdersF[Response] = for{
     _ <- buy("APPL", 50)
     _ <- buy("MSFT", 100)
-    _ <- sell("GOOG", 40)
     rsp <- buy("GOOG3", 41)
+    _ <- sell("GOOG", 40)
   } yield rsp
 
   def smartTraideWithList: Free[Orders, String] = for{
--- src/main/scala/com/test/orders/interpreters/OrdersErrorsExampleInterpreter.scala
+++ src/main/scala/com/test/orders/interpreters/OrdersErrorsExampleInterpreter.scala
@@ -7,12 +7,13 @@ import scala.util.Left
 
 object OrdersErrorsExampleInterpreter {
   def etheirInterpreter: Orders ~> ErrorOr =
-    new (Orders ~> ErrorOr){
+    new (Orders ~> ErrorOr) {
       def apply[A](fa: Orders[A]): ErrorOr[A] = fa match {
         case Buy(stock, amount) =>
           Right(s"""$stock - $amount""")
         case Sell(stock, amount) =>
-          Left("Why are you selling that?")
+          Left(s"""Why are you selling that? $stock""")
       }
     }
 }
+""
